68df113b6166fc6cc4e22880b14e853c2e0c6db6
Mario <mpontice@gmail.com>
2014-11-21 01:56:17 -0800

    Adjust gitignore; implement Header, MainSection, TodoApp, TodoItem, and TodoTextInput components

--- js/components/TodoApp.react.js
+++ js/components/TodoApp.react.js
@@ -1,35 +1,18 @@
-/**
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-/**
- * This component operates as a "Controller-View".  It listens for changes in
- * the TodoStore and passes the new data to its children.
- */
-
 var Footer = require('./Footer.react');
 var Header = require('./Header.react');
 var MainSection = require('./MainSection.react');
 var React = require('react');
 var TodoStore = require('../stores/TodoStore');
 
-/**
- * Retrieve the current TODO data from the TodoStore
- */
-function getTodoState() {
+// retrieve the current TODO data from the TodoStore
+var getTodoState = function() {
   return {
     allTodos: TodoStore.getAll(),
     areAllComplete: TodoStore.areAllComplete()
-  };
+  }
 }
 
 var TodoApp = React.createClass({
-
   getInitialState: function() {
     return getTodoState();
   },
@@ -42,25 +25,21 @@ var TodoApp = React.createClass({
     TodoStore.removeChangeListener(this._onChange);
   },
 
-  /**
-   * @return {object}
-   */
   render: function() {
-  	return (
+    return (
       <div>
         <Header />
         <MainSection
-          allTodos={this.state.allTodos}
+          allTodos = {this.state.allTodos}
           areAllComplete={this.state.areAllComplete}
         />
-        <Footer allTodos={this.state.allTodos} />
+        <Footer allTodos = {this.state.allTodos} />
       </div>
-  	);
+    );
   },
 
-  /**
-   * Event handler for 'change' events coming from the TodoStore
-   */
+  // event handler for 'change' events coming from TodoStore
+  // underscore must be a way to demarcate it as an event handler
   _onChange: function() {
     this.setState(getTodoState());
   }
